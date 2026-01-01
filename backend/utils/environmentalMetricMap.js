802721637da490161c8ca142200a63bae78b219a
V-shalini22 <shalinivarada12@gmail.com>
2026-01-01 11:01:13 +0530

    upadated by shalini

---
+++ backend/utils/environmentalMetricMap.js
@@ -0,0 +1,8 @@
+export const environmentalMetricMap = {
+  energyConsumption: "energy_consumption_mwh",
+  scope1Emissions: "ghg_scope1_tco2e",
+  waterDischarge: "water_discharge_m3",
+  industrialWaste: "mine_tailings_tonnes",
+  biodiversityImpact: "biodiversity_impact_score",
+  landRehabilitated: "land_rehabilitated_hectares",
+};
